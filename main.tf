95b52f98c1d21578c8fa4f5de6ed15ac4035e277
ignorabilis <irina.yaroslavova@ignorabilis.com>
2023-05-04 07:11:57 +0300

    try fix permission issues

--- main.tf
+++ main.tf
@@ -16,6 +16,25 @@ resource "aws_s3_bucket_website_configuration" "bucket" {
   }
 }
 
+# ACL & Policies are no longer working out of the box;
+# that's a temporary fix so that things at least run.
+resource "aws_s3_bucket_ownership_controls" "s3_bucket" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_bucket" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_acl" "bucket" {
   bucket = aws_s3_bucket.bucket.id
 
